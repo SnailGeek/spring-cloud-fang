4884f670d16756706cfac26012278abfa9b604bb
SnailGeek <codingsnail@163.com>
2019-10-29 21:58:40 +0800

    添加单独的hystrix dashboard

--- service-hi/src/main/java/com/forzep/servicehi/ServiceHiApplication.java
+++ service-hi/src/main/java/com/forzep/servicehi/ServiceHiApplication.java
@@ -2,10 +2,16 @@ package com.forzep.servicehi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class ServiceHiApplication {
 
     public static void main(String[] args) {
--- service-hi/src/main/java/com/forzep/servicehi/controller/HelloController.java
+++ service-hi/src/main/java/com/forzep/servicehi/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.forzep.servicehi.controller;
 
 import com.forzep.servicehi.service.HelloService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +14,12 @@ public class HelloController {
     HelloService helloService;
 
     @GetMapping("/hi")
+    @HystrixCommand(fallbackMethod = "hiError")
     public String hello(@RequestParam(value = "name", defaultValue = "forzep") String name){
         return helloService.Hello(name);
+    }
+
+    public String hiError(String name){
+        return "hi, " + name + "sorry error!";
     }
 }
