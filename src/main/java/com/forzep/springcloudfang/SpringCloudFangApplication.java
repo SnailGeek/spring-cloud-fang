c0713293d870a97307800aa2ed317cdb648d9e4c
SnailGeek <codingsnail@163.com>
2019-10-29 20:43:45 +0800

    高可用注册中心（未成功）

---
+++ src/main/java/com/forzep/springcloudfang/SpringCloudFangApplication.java
@@ -0,0 +1,11 @@
+package com.forzep.springcloudfang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudFangApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudFangApplication.class, args);
+    }
+}
